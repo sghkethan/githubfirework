746426e40a04d06701a8e796b6c059e9f1a930ef
sghkethan <sghkethan@outlook.com>
2025-05-30 17:41:27 +0800

    Update talk.js

--- talk.js
+++ talk.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#yes').click(function(event) {
-        modal('这里是李炳绪女友的专属区域(#^.^#)', function() {
+        modal('这里是lbx女友的专属区域(#^.^#)', function() {
             $('.page_one').addClass('hide');
             $('.page_two').removeClass('hide');
             // typeWrite();
@@ -14,7 +14,7 @@ $(function() {
 });
 
 function A() {
-    modal('晚了晚了 点了这个就会变成李炳绪小哥哥的女朋友', B);
+    modal('晚了晚了 点了这个就会变成lbx小哥哥的女朋友', B);
 }
 
 function B() {
